8ee1181ffe2e96ba2b49051607c709021bfcdd51
Shahahdat <raselsha@gmail.com>
2021-08-08 18:24:37 +0600

    finished

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,131 +1,142 @@
+// play button id list
+var btn=['btn1','btn2','btn3','btn4','btn5','btn6','btn7','btn8','btn9','btn10','btn11','btn12','btn13','btn14','btn15','btn16','btn17','btn18','btn19','btn20'];
+// sound list in assets/audio/*.mp3
+var sound=['sound1','sound2','sound3','sound4','sound5','sound6','sound7','sound8','sound9','sound10','sound11','sound12','sound13','sound14','sound15','sound16','sound17','sound18','sound19','sound20'];
+// left table letter ID list
+var charA=['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13','a14','a15','a16','a17','a18','a19','a20'];
+// right table letter ID list
+var charB=['b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20'];
 
-var sound=['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10'];
-var audio=['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'];
 
 var player = document.getElementById('player');
 var i=0; //increment
 
-reset(); // reset page for all 
-start(); // start the program
+// reset table letters 
+reset(); 
+
+// start the program
+start(); 
 
 // this method will reset elements
 function reset(){
-	for (var i = 0; i < 10; i++) {
-	  	inactive(mic[i]); // inactive microphone icons
-	  	inactive(sound[i]); // inactive user sounds icon
-	  	hide(audio[i]); // hide original sounds icon
+	for (var i = 0; i < 20; i++) {
+	  	hide(charA[i]); // hide left table letters
+	  	show(charB[i]); // show right table letters
+	  	removePlaySoundBtn(btn[i]); //remove paly sound from left table
+	  	inactiveBtn(btn[i]);  // play button deactive
+
+	  	addNoSoundBtn(charB[i]); // add no sound btn to left table
+	  	activeChar(charB[i]);  // active left table letters
 	}
 }
 
 function start() {
 	if(i==0){
-	  wordActive(words[i]); // make first word green as active
-	  addRecordingBtn(mic[i]); // add attribute onmousedown for recording
-	  active(mic[i]); // active for recording buton
+	   activeBtn(btn[i]); // make first button green and active 
+	   addPlayNowBtn(btn[i]);
 	}
 }
 
+function playNow(){
+	playSound(sound[i]);
+	var charElemA  = document.getElementById(charA[i]);
+	for (var j = 0; j < 20; j++) {
+		var charElemB  = document.getElementById(charB[j]);
+		if(charElemA.innerHTML==charElemB.innerHTML){
+			removeNoSoundBtn(charElemB.id);
+			var element = document.getElementById(charElemB.id);
+			var att = document.createAttribute("onclick");
+			att.value = "moveNow("+charB[j]+","+charA[i]+")";
+			element.setAttributeNode(att);
+		}
+	}
+}
 
-function recordStart(id) {
+function moveNow(source,desti){
+	playSound(sound[i]);
+    var src = source.getBoundingClientRect();
+    var dest = desti.getBoundingClientRect();
+    var left = src.x;
+    var top = src.y;
+    source.style.transition = 'all 1s ease-in-out';
+    source.style.transform = 'translate('+(dest.x-src.x)+'px,'+(dest.y-src.y)+'px)';
+    setTimeout(function(){
+         resetTo(source,desti); // move letter to original position
+      }, 1000);
+}
+
+function resetTo(source,desti) {
+      show(desti.id);
+      hide(source.id);
+      source.removeAttribute('style');
+      source.classList.remove('pointer');
+      source.removeAttribute("onclick");
+      makeNormalBtn(btn[i]);
+      next(); 
+}
+
+function next(){
+	i++;
+	if(i==20){
+		i=0;
+		endLesson();
+	}
+	else{
+		activeBtn(btn[i]); // make first button green and active 
+		addPlayNowBtn(btn[i]);
+	}
+}
+
+function addPlayNowBtn(id){
 	var element = document.getElementById(id);
-	var screen = window.matchMedia("(max-width:991px)");
-	if (screen.matches) { // If media query matches
-	    element.style.backgroundPosition='-30px';
-	  } else {
-	   element.style.backgroundPosition='-40px';
-	  }
-	//+++++++++ recording funciton +++++++++
-  // recording(); // manual function for record audio stream
-  recording(id); // adapter recording;
-  addRecordingStopBtn(id);
-  inactive(sound[i]);
-}
-
-//adapter reacorder
-function recording(){
-	const audioStream = async function(stream){
-		config = {type:'audio',mimeType:'audio/webm'}
-		recorder = RecordRTC(stream,config);
-		recorder.startRecording();
-
-	};
-
-	navigator.mediaDevices.getUserMedia({video: true,audio: true}).then(audioStream);
-}
-
-
-function recordStop(id) {
-	var element = document.getElementById(id);
-	element.style.backgroundPosition='0px';
-	
-	recorder.stopRecording(function() {
-		recorded_data[i] = recorder.toURL();
-	    console.log("audio["+i+"]"+recorded_data[i]);
-	});
-
-	addPlaySoundBtn(sound[i]);
-	active(sound[i]);
-
-	removeRecordingBtn(mic[i]);
-	wordInactive(words[i]);
-	inactive(mic[i]);
-
-	setTimeout(function(){
-		nextWord();	
-	},100);
-	
-}
-
-function addPlaySoundBtn(id){
-	var sound = document.getElementById(id);
 	var att = document.createAttribute("onclick");
-	att.value = "playUserSound('"+id+"')";
-	sound.setAttributeNode(att);
+	att.value = "playNow()";
+	element.setAttributeNode(att);
 }
 
-function playUserSound(id){
-	player.src = recorded_data[sound.indexOf(id)];
-	player.play();	
-}
-
-function addRecordingBtn(id){
+function removePlayNowBtn(id){
 	var element = document.getElementById(id);
-	var onclick = document.createAttribute("onclick");
-	onclick.value = "recordStart('"+id+"')";
-	element.setAttributeNode(onclick);
+	var att = document.createAttribute("onclick");
+	att.value = "playSound('"+sound[btn.indexOf(id)]+"')";
+	element.setAttributeNode(att);
 }
 
-function addRecordingStopBtn(id){
+function addNoSoundBtn(id){
 	var element = document.getElementById(id);
-	var onclick = document.createAttribute("onclick");
-	onclick.value = "recordStop('"+id+"')";
-	element.setAttributeNode(onclick);
+	var att = document.createAttribute("onclick");
+	att.value = "playNoSound()";
+	element.setAttributeNode(att);
 }
 
-function removeRecordingBtn(id){
+function removeNoSoundBtn(id){
 	var element = document.getElementById(id);
 	element.removeAttribute('onclick');
 }
 
-function nextWord(){
-	i++;
-	if (i<10) {
-		wordActive(words[i]);
-		addRecordingBtn(mic[i]);
-		active(mic[i]);
-	}
-	else{
-		 setTimeout(function(){
-			activeOriginalAudio();
-		},1500);
-	}
-	
+function addPlaySoundBtn(id){
+	var element = document.getElementById(id);
+	var att = document.createAttribute("onclick");
+	att.value = "playSound('"+sound[btn.indexOf(id)]+"')";
+	element.setAttributeNode(att);
 }
 
-function playOriginalAudio(id) {	
-	player.src = 'assets/audio/'+id+'.mp3';
-	player.play();
+function removePlaySoundBtn(id){
+	var element = document.getElementById(id);
+	element.removeAttribute('onclick');
 }
+
+function playSound(id){
+	player.src = 'assets/audio/'+id+'.mp3';
+	player.play();	
+}
+
+function playNoSound(){
+	var number = Math.floor(Math.random()*3)+1;
+	console.log(number);
+	player.src = 'assets/audio/no'+number+'.mp3';
+	player.play();	
+}
+
 
 function wordActive(id){
 	var h1 = document.getElementById(id);
@@ -145,17 +156,43 @@ function show(id){
 }
 function hide(id){
 	var item = document.getElementById(id);
+	item.classList.remove('visible');
 	item.classList.add('invisible');
 }
-function active(id){
+
+function activeChar(id){
 	var item = document.getElementById(id);
 	item.style.filter= 'grayscale(0%)';
+	item.style.opacity= '1';
 	item.classList.add('pointer');
 }
-function inactive(id){
+function inactiveChar(id){
 	var item = document.getElementById(id);
 	item.style.filter= 'grayscale(100%)';
+	item.style.opacity= '0.5';
 	item.classList.remove('pointer');
+}
+
+function activeBtn(id){
+	var item = document.getElementById(id);
+	item.src = 'assets/images/play-green.svg';
+	item.style.filter= 'grayscale(0%)';
+	item.style.opacity= '1';
+	item.classList.add('pointer');
+}
+function inactiveBtn(id){
+	var item = document.getElementById(id);
+	item.src = 'assets/images/play-white.svg';
+	item.style.filter= 'grayscale(100%)';
+	item.style.opacity= '0.5';
+	item.classList.remove('pointer');
+}
+function makeNormalBtn(id){
+	var item = document.getElementById(id);
+	item.src = 'assets/images/play-white.svg';
+	item.style.filter= 'grayscale(0%)';
+	item.style.opacity= '1';
+	addPlaySoundBtn(id);
 }
 
 function endLesson() {
